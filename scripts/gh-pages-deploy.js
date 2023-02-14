00ebf93712c00ce35e9d79839b53193c8edd18a1
Unseenfaith <1832323+UnseenFaith@users.noreply.github.com>
2023-02-13 22:37:45 -0500

    fix script

--- scripts/gh-pages-deploy.js
+++ scripts/gh-pages-deploy.js
@@ -16,9 +16,7 @@ const thirdLog = `${emoji.get("rocket")} ${chalk.green(
     await execa.command("git checkout --orphan gh-pages");
     console.log(firstLog);
     await execa.command("npm run build", { stdio: "inherit" });
-    await execa.command("cd dist");
-    await execa.command("cp index.html 404.html");
-    await execa.command("cd ../");
+    await execa.command("copy ./dist/index.html ./dist/404.html");
     await execa.command("git --work-tree dist add --all");
     await execa.command('git --work-tree dist commit -m "gh-pages"');
     console.log(secondLog);
